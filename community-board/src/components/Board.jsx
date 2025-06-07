eeecf51d2c95a5710820262594c8e75e25454ad5
Kelvin Mathew <kelvinmathew2004@gmail.com>
2025-06-07 18:15:22 -0500

    Added more movies and effects

--- community-board/src/components/Board.jsx
+++ community-board/src/components/Board.jsx
@@ -3,26 +3,36 @@ import Movie from './Movie'
 function Board() {
   return (
     <div className="board">
+        <Movie image="https://image.tmdb.org/t/p/original/edKpE9B5qN3e559OuMCLZdW1iBZ.jpg" title="Mickey 17" releaseDate="7 March" trailerLink="https://youtube.com/watch?v=osYpGSz_0i4"/>
+        <Movie image="https://image.tmdb.org/t/p/original/yFHHfHcUgGAxziP1C3lLt0q2T4s.jpg" title="Minecraft" releaseDate="4 April" trailerLink="https://youtube.com/watch?v=wJO_vIDZn-I"/>
+        <Movie image="https://image.tmdb.org/t/p/original/yqsCU5XOP2mkbFamzAqbqntmfav.jpg" title="Sinners" releaseDate="18 April" trailerLink="https://youtube.com/watch?v=bKGxHflevuk"/>
+        <Movie image="https://image.tmdb.org/t/p/original/kMDUS7VmFhb2coRfVBoGLR8ADBt.jpg" title="The Accountant 2" releaseDate="25 April" trailerLink="https://youtube.com/watch?v=3wRCOqyDI6E"/>
+        <Movie image="https://image.tmdb.org/t/p/original/vnfgoohSwKNOcRfJOPULXTvX0cu.jpg" title="Thunderbolts*" releaseDate="2 May" trailerLink="https://youtube.com/watch?v=-sAOWhvheK8"/>
+        <Movie image="https://image.tmdb.org/t/p/original/6WxhEvFsauuACfv8HyoVX6mZKFj.jpg" title="Final Destination Bloodlines" releaseDate="16 May" trailerLink="https://youtube.com/watch?v=UWMzKXsY9A4"/>
+        <Movie image="https://image.tmdb.org/t/p/original/z53D72EAOxGRqdr7KXXWp9dJiDe.jpg" title="Mission: Impossible - The Final Reckoning" releaseDate="23 May" trailerLink="https://youtube.com/watch?v=NOhDyUmT9z0"/>
+        <Movie image="https://image.tmdb.org/t/p/original/tUae3mefrDVTgm5mRzqWnZK6fOP.jpg" title="Lilo & Stitch" releaseDate="23 May" trailerLink="https://www.youtube.com/watch?v=VWqJifMMgZE"/>
+        <Movie image="https://image.tmdb.org/t/p/original/c90Lt7OQGsOmhv6x4JoFdoHzw5l.jpg" title="Karate Kid: Legends" releaseDate="30 May" trailerLink="https://youtube.com/watch?v=uPzOyzsnmio"/>
         <Movie image="https://image.tmdb.org/t/p/original/mKp4euM5Cv3m2U1Vmby3OGwcD5y.jpg" title="Ballerina" releaseDate="6 June" trailerLink="https://youtube.com/watch?v=0FSwsrFpkbw"/>
         <Movie image="https://image.tmdb.org/t/p/original/3lwlJL8aW6Wor9tKvME8VoMnBkn.jpg" title="How to Train Your Dragon" releaseDate="13 June" trailerLink="https://youtube.com/watch?v=22w7z_lT6YM"/>
         <Movie image="https://image.tmdb.org/t/p/original/hVKHzr4GwSw0FepqhqQ0DDiYHNY.jpg" title="28 Years Later" releaseDate="19 June" trailerLink="https://youtube.com/watch?v=mcvLKldPM08"/>
         <Movie image="https://image.tmdb.org/t/p/original/iGyqJwqAHQjRrChcOCo6Nqgkb0B.jpg" title="F1 The Movie" releaseDate="27 June" trailerLink="https://youtube.com/watch?v=8yh9BPUBbbQ"/>
+        <Movie image="https://image.tmdb.org/t/p/original/hq0ONyCxMNirXetaEjDY7iOp29p.jpg" title="Megan 2.0" releaseDate="27 June" trailerLink="https://youtube.com/watch?v=IYLHdEzsk1s"/>
         <Movie image="https://image.tmdb.org/t/p/original/q0fGCmjLu42MPlSO9OYWpI5w86I.jpg" title="Jurassic World Rebirth" releaseDate="2 July" trailerLink="https://youtube.com/watch?v=jan5CFWs9ic"/>
         <Movie image="https://image.tmdb.org/t/p/original/qjVMsjV7UmSpi1aHA9Wtl1v8cxr.jpg" title="The Old Guard 2" releaseDate="2 July" trailerLink="https://youtube.com/watch?v=0FSwsrFpkbw"/>
         <Movie image="https://image.tmdb.org/t/p/original/wPLysNDLffQLOVebZQCbXJEv6E6.jpg" title="Superman" releaseDate="6 June" trailerLink="https://youtube.com/watch?v=Ox8ZLF6cGM0"/>
         <Movie image="https://image.tmdb.org/t/p/original/x26MtUlwtWD26d0G0FXcppxCJio.jpg" title="The Fantastic Four: First Steps" releaseDate="25 July" trailerLink="https://youtube.com/watch?v=pAsmrKyMqaA"/>
         <Movie image="https://image.tmdb.org/t/p/original/rqgqHT7S6VTXPSkbNFb2kihc3tL.jpg" title="The Bad Guys 2" releaseDate="1 August" trailerLink="https://youtube.com/watch?v=TY1lWh20VSw"/>
         <Movie image="https://image.tmdb.org/t/p/original/iyxwxDZCpIm0vIORaHpmgJv2BGF.jpg" title="Nobody 2" releaseDate="15 August" trailerLink="https://youtube.com/watch?v=-5X2pt95cIo"/>
+        <Movie image="https://image.tmdb.org/t/p/original/29ES27icY5CzTcMhlz1H4SdQRod.jpg" title="The Conjuring: Last Rites" releaseDate="5 September" trailerLink="https://youtube.com/watch?v=FSAz556s0fM"/>
         <Movie image="https://image.tmdb.org/t/p/original/chpWmskl3aKm1aTZqUHRCtviwPy.jpg" title="Tron Ares" releaseDate="10 October" trailerLink="https://youtube.com/watch?v=9KVG_X_7Naw"/>
         <Movie image="https://image.tmdb.org/t/p/original/oD3Eey4e4Z259XLm3eD3WGcoJAh.jpg" title="Now You See Me: Now You Don't" releaseDate="14 November" trailerLink="https://youtube.com/watch?v=-E3lMRx7HRQ"/>
-        <Movie image="https://image.tmdb.org/t/p/original/vAtyR5471YBzBb2FrCiCTrxwv3E.jpg" title="Zootopia 2" releaseDate="6 June" trailerLink="https://youtube.com/watch?v=xo4rkcC7kFc"/>
+        <Movie image="https://image.tmdb.org/t/p/original/si9tolnefLSUKaqQEGz1bWArOaL.jpg" title="Wicked: For Good" releaseDate="21 November" trailerLink="https://youtube.com/watch?v=vt98AlBDI9Y"/>
+        <Movie image="https://image.tmdb.org/t/p/original/vAtyR5471YBzBb2FrCiCTrxwv3E.jpg" title="Zootopia 2" releaseDate="26 November" trailerLink="https://youtube.com/watch?v=xo4rkcC7kFc"/>
+        <Movie image="https://image.tmdb.org/t/p/original/brJkq22WjpLnCuYEEId89FC3NXZ.jpg" title="Five Nights at Freddy’s 2" releaseDate="5 December" trailerLink="https://youtube.com/watch?v=eGV7zwjvxKs"/>
+        <Movie image="https://image.tmdb.org/t/p/original/kT4L65t5Jcjune7zEoLa8AxyhkL.jpg" title="Wake Up Dead Man: A Knives Out Mystery" releaseDate="12 December" trailerLink="https://youtube.com/watch?v=sRqU2276eaQ"/>
+        <Movie image="https://image.tmdb.org/t/p/original/vDdFxksDMkUJjZXd00OKJfwXKre.jpg" title="Avatar: Fire and Ash" releaseDate="19 December" trailerLink="https://youtube.com/watch?v=6BPFz5nHgog"/>
     </div>
   )
 }
 
 export default Board
-
-{/* <img src={props.image} alt={props.title} />
-      <h2>{props.title}</h2>
-      <p>{props.releaseDate}</p>
-      <a href={props.trailerLink} className="button"></a> */}
